9af542cbdff382d13a5096bb0b1a79145968f06f
prasanthreddylomada <lomadaprasanthreddy@gmail.com>
2023-06-25 19:15:26 +0530

    added landing page and flow logo

---
+++ lib/Landingpage/landingpage.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'To the Secure payments platform',
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: mediaQuery.size.height*0.05),
+            Image.asset(
+              'Assets/flow_logo.svg', // Replace with your image path
+              width: mediaQuery.size.width * 0.5, // Adjust the width as needed
+              height: mediaQuery.size.height * 0.2, // Adjust the height as needed
+            ),
+            Container(
+                width: mediaQuery.size.width * 0.8, 
+                height: mediaQuery.size.height * 0.06,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle sign-up button press
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(mediaQuery.size.height * 0.03),
+                    ),
+                  ),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+
+            SizedBox(height: mediaQuery.size.height*0.02),
+           Container(
+                width: mediaQuery.size.width * 0.8, 
+                height: mediaQuery.size.height * 0.06,
+                child: OutlinedButton(
+                  onPressed: () {
+                    // Handle login button press
+                  },
+                  style: OutlinedButton.styleFrom(
+                    
+                    side: BorderSide(color: Colors.green),
+                    backgroundColor: Colors.green,
+                    primary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(mediaQuery.size.height * 0.03),
+                    ),
+                  ),
+                  child: Text('Login',style: TextStyle(fontSize: 20),),
+                ),
+              ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
