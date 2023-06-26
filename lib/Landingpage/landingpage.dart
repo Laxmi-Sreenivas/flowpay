a5cc937f411b7222daed40dac925b9a84e384bc0
Laxmi-Sreenivas <lakshmisreenivasmail@gmail.com>
2023-06-26 14:38:33 +0530

    minor change

--- lib/Landingpage/landingpage.dart
+++ lib/Landingpage/landingpage.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import "package:flowpay/Assets/flow_logo.svg";
+import 'package:flowpay/Signup/signup.dart';
+import 'package:flowpay/Login/loginpage.dart';
 
 class LandingPage extends StatelessWidget {
+
+  void toSignup(BuildContext context){
+        Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) => SignupPage()),
+        (Route<dynamic> route) => false);
+  }
+
+    void toLogin(BuildContext context){
+        Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) => LoginPage()),
+        (Route<dynamic> route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     final MediaQueryData mediaQuery = MediaQuery.of(context);
+    double w = mediaQuery.size.width;
+    double h = mediaQuery.size.height;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -15,69 +33,78 @@ class LandingPage extends StatelessWidget {
             Text(
               'Welcome',
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: w*0.1,
+                fontWeight: FontWeight.w900,
                 color: Colors.white,
               ),
             ),
+            SizedBox(height: mediaQuery.size.height * 0.01),
             Text(
               'To the Secure payments platform',
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: w*0.04,
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: mediaQuery.size.height*0.05),
-            SvgPicture.asset(
-              'Assets/flow_logo.svg', // Replace with your SVG file path
-              width: mediaQuery.size.width * 0.5, // Adjust the width as needed
-              height: mediaQuery.size.height * 0.2, // Adjust the height as needed
+            SizedBox(height: mediaQuery.size.height * 0.05),
+            Container(
+              margin: EdgeInsets.fromLTRB(0.01*w,h*0.01,0.01*w,h*0.06),
+              child: Image.asset(
+                "lib/Assets/flow_logo.png", // Replace with your SVG file path
+                width:
+                    mediaQuery.size.width * 0.5, // Adjust the width as needed
+                height:
+                    mediaQuery.size.height * 0.3, // Adjust the height as needed
+              ),
             ),
             Container(
-                width: mediaQuery.size.width * 0.8, 
-                height: mediaQuery.size.height * 0.06,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Handle sign-up button press
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.black),
-                      borderRadius: BorderRadius.circular(mediaQuery.size.height * 0.03),
-                    ),
+              width: mediaQuery.size.width * 0.8,
+              height: mediaQuery.size.height * 0.07,
+              child: ElevatedButton(
+                onPressed: () {
+                  toSignup(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(color: Colors.black),
+                    borderRadius:
+                        BorderRadius.circular(mediaQuery.size.height * 0.03),
                   ),
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                    ),
+                ),
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize:  w*0.055,
                   ),
                 ),
               ),
-
-            SizedBox(height: mediaQuery.size.height*0.02),
-           Container(
-                width: mediaQuery.size.width * 0.8, 
-                height: mediaQuery.size.height * 0.06,
-                child: OutlinedButton(
-                  onPressed: () {
-                    // Handle login button press
-                  },
-                  style: OutlinedButton.styleFrom(
-                    
-                    side: BorderSide(color: Colors.green),
-                    backgroundColor: Colors.green,
-                    primary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(mediaQuery.size.height * 0.03),
-                    ),
+            ),
+            SizedBox(height: mediaQuery.size.height * 0.04),
+            Container(
+              width: mediaQuery.size.width * 0.8,
+              height: mediaQuery.size.height * 0.07,
+              child: OutlinedButton(
+                onPressed: () {
+                  toLogin(context);
+                },
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.green),
+                  backgroundColor: Colors.green,
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(mediaQuery.size.height * 0.03),
                   ),
-                  child: Text('Login',style: TextStyle(fontSize: 20),),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: w*0.055),
                 ),
               ),
-
+            ),
           ],
         ),
       ),
