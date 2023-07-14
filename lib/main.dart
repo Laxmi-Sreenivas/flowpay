b1b329f6cfa9fb11e7296b058ba0e3b557ea7173
SuryaVamsi23 <suryavamsikalaga@gmail.com>
2023-07-14 23:27:03 +0530

    Added firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flowpay/Landingpage/landingpage.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const FlowPay());
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlowPay());
 }
-
 class FlowPay extends StatelessWidget {
   const FlowPay({super.key});
 
